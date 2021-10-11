4f7be9f7b4c53585c0295b3cd26d00d5ec3408fb
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-10-11 21:42:34 +0700

    update 11/10

---
+++ internet-service-management/src/app/service/model/IService.ts
@@ -0,0 +1,8 @@
+export interface IService {
+id: number;
+serviceId:string;
+serviceName:string;
+quantity:number;
+unit: number;
+price:number;
+}
--- internet-service-management/src/app/service/service.module.ts
+++ internet-service-management/src/app/service/service.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ServiceListComponent } from './service-list/service-list.component';
+import { ServiceDeleteComponent } from './service-delete/service-delete.component';
 
 @NgModule({
-  declarations: [],
+    declarations: [
+        ServiceListComponent,
+        ServiceDeleteComponent
+    ],
+    exports: [
+        ServiceListComponent
+    ],
   imports: [
     CommonModule
   ]
