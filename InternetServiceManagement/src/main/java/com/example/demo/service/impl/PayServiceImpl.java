41d17dfd2980a1dab0ae2c6e9dd88945cca2b710
A1020I1-NgoHienNhan <44870954+NgoHienNhan@users.noreply.github.com>
2021-10-11 21:43:57 +0700

    Delete InternetServiceManagement directory

--- InternetServiceManagement/src/main/java/com/example/demo/service/impl/PayServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.service.PayService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class PayServiceImpl implements PayService {
-}
