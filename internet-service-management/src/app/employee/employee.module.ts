5b1a309ff48107915fb43b2d3dbe1f1c227d741e
NHNJoker <ngonhanit@gmail.com>
2021-10-22 02:04:07 +0700

    update

--- internet-service-management/src/app/employee/employee.module.ts
+++ internet-service-management/src/app/employee/employee.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TestComponent } from './test/test.component';
 import {CustomerModule} from "../customer/customer.module";
 
 
 
 @NgModule({
   declarations: [
-    TestComponent
   ],
   exports: [
-    TestComponent
   ],
   imports: [
     CommonModule,
-    CustomerModule
   ]
 })
 export class EmployeeModule { }
