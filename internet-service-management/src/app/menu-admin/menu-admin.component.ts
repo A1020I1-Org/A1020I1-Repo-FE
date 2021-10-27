702ce5508e859226cdba3c46803dab4d92d178f0
Jwcfree01 <thonglntbda180011@fpt.edu.vn>
2021-10-27 16:01:55 +0700

    thong

--- internet-service-management/src/app/menu-admin/menu-admin.component.ts
+++ internet-service-management/src/app/menu-admin/menu-admin.component.ts
@@ -8,11 +8,19 @@ import {LoadJsService} from "../service/load-js.service";
 })
 export class MenuAdminComponent implements OnInit {
 
+  employeeLogined!: any;
+
   constructor(private loadjs: LoadJsService) {
     this.loadjs.loadScript('assets/js/menu_left.js');
   }
 
   ngOnInit(): void {
+    // this.employeeLogined = this.tokenStorage.getUser().employee;
+  }
+
+  signOut() {
+    // this.tokenStorage.signOut();
+    // this.router.navigateByUrl('/login');
   }
 
 }
